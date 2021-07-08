dcf09141f7b8ba47c418391cf76850967f1599d7
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-07-08 15:24:25 +0530

    Initial Commit

---
+++ src/main/java/com/capeelectric/CapeElectricPaymentGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.capeelectric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapeElectricPaymentGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapeElectricPaymentGatewayApiApplication.class, args);
+	}
+
+}
