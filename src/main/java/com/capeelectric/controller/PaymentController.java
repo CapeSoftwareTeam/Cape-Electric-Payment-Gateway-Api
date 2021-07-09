e1e67b8b76754e44c5c5f59ad2e03c3b852dab6e
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-07-09 19:39:56 +0530

    MOved the controller to REstCOntroller

--- src/main/java/com/capeelectric/controller/PaymentController.java
+++ src/main/java/com/capeelectric/controller/PaymentController.java
@@ -9,17 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.capeelectric.model.PaytmDetailPojo;
 import com.paytm.pg.merchant.PaytmChecksum;
 
-@Controller
+@RestController()
+@RequestMapping("/api/v1")
 public class PaymentController {
 	
 	@Autowired
